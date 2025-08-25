dd80dfcba8db46b04b387cf57a0fca00423c8382
Kristjan Kelk <60584291+KristjanKelk@users.noreply.github.com>
2025-08-26 01:08:59 +0300

    Revert "Find closest and cheapest parking in Tallinn"

--- parkinfo/src/main.js
+++
@@ -1,9 +0,0 @@
-import { createApp } from 'vue'
-import './styles.css'
-import 'leaflet/dist/leaflet.css'
-import App from './App.vue'
-import router from './router'
-import { registerSW } from 'virtual:pwa-register'
-
-registerSW({ immediate: true })
-createApp(App).use(router).mount('#app')
